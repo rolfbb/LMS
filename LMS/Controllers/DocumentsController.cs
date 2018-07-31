160914281aed8cb536eb2a902ddd6aaa0e61bf6e
alisalhab19861986 <alisalhab19861986@gmail.com>
2018-07-31 13:56:22 +0200

    comment edit

--- LMS/Controllers/DocumentsController.cs
+++ LMS/Controllers/DocumentsController.cs
@@ -181,42 +181,42 @@ namespace LMS.Controllers
 
 		
 
-		// GET: Documents/Edit/5
-		public ActionResult Edit(int? id)
-		{
-			if (id == null)
-			{
-				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-			}
-			Document document = db.Documents.Find(id);
-			if (document == null)
-			{
-				return HttpNotFound();
-			}
-			ViewBag.ActivityId = new SelectList(db.Activities, "Id", "Name", document.ActivityId);
-			ViewBag.CourseId = new SelectList(db.Courses, "Id", "Name", document.CourseId);
-			ViewBag.ModuleId = new SelectList(db.Modules, "Id", "Name", document.ModuleId);
-			return View(document);
-		}
+		//// GET: Documents/Edit/5
+		//public ActionResult Edit(int? id)
+		//{
+		//	if (id == null)
+		//	{
+		//		return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+		//	}
+		//	Document document = db.Documents.Find(id);
+		//	if (document == null)
+		//	{
+		//		return HttpNotFound();
+		//	}
+		//	ViewBag.ActivityId = new SelectList(db.Activities, "Id", "Name", document.ActivityId);
+		//	ViewBag.CourseId = new SelectList(db.Courses, "Id", "Name", document.CourseId);
+		//	ViewBag.ModuleId = new SelectList(db.Modules, "Id", "Name", document.ModuleId);
+		//	return View(document);
+		//}
 
-		// POST: Documents/Edit/5
-		// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-		// more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-		[HttpPost]
-		[ValidateAntiForgeryToken]
-		public ActionResult Edit([Bind(Include = "Id,Name,Description,TimeStamp,OwnerId,CourseId,ModuleId,ActivityId")] Document document)
-		{
-			if (ModelState.IsValid)
-			{
-				db.Entry(document).State = EntityState.Modified;
-				db.SaveChanges();
-				return RedirectToAction("Index");
-			}
-			ViewBag.ActivityId = new SelectList(db.Activities, "Id", "Name", document.ActivityId);
-			ViewBag.CourseId = new SelectList(db.Courses, "Id", "Name", document.CourseId);
-			ViewBag.ModuleId = new SelectList(db.Modules, "Id", "Name", document.ModuleId);
-			return View(document);
-		}
+		//// POST: Documents/Edit/5
+		//// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+		//// more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+		//[HttpPost]
+		//[ValidateAntiForgeryToken]
+		//public ActionResult Edit([Bind(Include = "Id,Name,Description,TimeStamp,OwnerId,CourseId,ModuleId,ActivityId")] Document document)
+		//{
+		//	if (ModelState.IsValid)
+		//	{
+		//		db.Entry(document).State = EntityState.Modified;
+		//		db.SaveChanges();
+		//		return RedirectToAction("Index");
+		//	}
+		//	ViewBag.ActivityId = new SelectList(db.Activities, "Id", "Name", document.ActivityId);
+		//	ViewBag.CourseId = new SelectList(db.Courses, "Id", "Name", document.CourseId);
+		//	ViewBag.ModuleId = new SelectList(db.Modules, "Id", "Name", document.ModuleId);
+		//	return View(document);
+		//}
 
 		// GET: Documents/Delete/5
 		public ActionResult Delete(int? id)
